cce0e03488f966f8d0febe7cacd2fbf8b8bc73c8
Megg Gawat <megawatt@live.com>
2018-03-22 11:50:51 -0400

    JS Runs when run button is pressed

--- scripts.js
+++ scripts.js
@@ -28,12 +28,21 @@ function changeHTML(html) {
     $('#results').contents().find('html').html(html);
 }
 
+function runJS() {
+    // WARNING: The way this function runs is dangerous as it allows
+    // ANY JS to be run. Remove this function or recode it before
+    // Uploading to your website.
+    let js = $('#JScode').val();
+    document.getElementById('results').contentWindow.eval(js);
+}
+
 function runCode() {
     $('#run').click(function() {
         let html = $('#HTMLcode').val();
         let css = '<style>' + $('#CSScode').val() + '</style>';
         html = css + html;
         changeHTML(html);
+        runJS();
     });
 }
 runCode();
